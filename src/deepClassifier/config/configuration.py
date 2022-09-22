c32e3509cee4fbe4bd6067c220a753519b9b32b4
sonupatro <sonupatro555@gmail.com>
2022-09-22 13:16:02 +0530

    config added

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -10,17 +10,15 @@ class ConfigurationManager:
         params_filepath = PARAMS_FILE_PATH):
         self.configs = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
-        create_directories([self.config.artifacts_root])
+        create_directories([self.configs.artifacts_root])
 
-        def get_data_ingestion_config(self) -> DataIngestionConfig:
-            config = self.configs.data_ingestion
-
-            create_directories([config.root_dir])
-
-            data_ingestion_config = DataIngestionConfig(
-                root_dir = config.root_dir,
-                source_url = config.source_URL,
-                local_data_file = config.local_data_file,
-                unzip_dir = config.unzip_dir
-            )
-            return data_ingestion_config
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.configs.data_ingestion
+        create_directories([config.root_dir])
+        data_ingestion_config = DataIngestionConfig(
+        root_dir = config.root_dir,
+        source_url = config.source_URL,
+        local_data_file = config.local_data_file,
+        unzip_dir = config.unzip_dir
+        )
+        return data_ingestion_config
